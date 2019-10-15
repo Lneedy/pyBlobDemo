2be2bcd9dff024d2eb9c3df1b4e9217e04493d45
lneedy <1296570868@qq.com>
2019-10-15 10:26:37 +0800

    feat(pyBlobDemo): Day 6 编写 配置文件 添加 product develop 配置信息

---
+++ www/config_default.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+develop config
+'''
+
+__Author__ = 'lneedy'
+
+config = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www-data',
+        'password': 'www-data',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+
+}
---
+++ www/config_override.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+product config
+'''
+
+__Author__ = 'lneedy'
+
+configs = {
+    'db': {
+        'host': '192.168.0.100'
+    }
+}
